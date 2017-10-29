8d8856459f9f4acdd528816efa8df19b4673c7f8
vmiis <zzm.vmiis@gmail.com>
2017-10-29 21:11:36 +1100

    update

--- modules/dynamic/bookkeeping/transaction_reporting/bookkeeping_details.js
+++ modules/dynamic/bookkeeping/transaction_reporting/bookkeeping_details.js
@@ -1,13 +1,13 @@
 var fields="Date,Name,Description,Transaction Type|I2_IE,Tax|I2_Tax_Rate,Item of Tax Return|I2_Tax_Return,Accounting Code|I2_Accounting_Code,Amount";
 _fields="_Form,"+fields;
 //-------------------------------------
-var param=_sys.config.parameters;
-var predefined_item_tid=param.predefined_item_tid;
+var prefix=_mlist[$vm.vm['__ID'].name].prefix;
+var predefined_transaction_item_tid=_mlist[prefix+'predefined_transaction_item'].table_id;
 //-------------------------------------
 $('#name__ID').autocomplete({
     minLength:0,
     source:function(request,response){
-        var sql="with tb as (select name=S1 from [TABLE-"+predefined_item_tid+"])";
+        var sql="with tb as (select name=S1 from [TABLE-"+predefined_transaction_item_tid+"])";
         sql+=" select top 10 name,value=name from tb where name like '%'+@S1+'%' ";
         $VmAPI.request({data:{cmd:'auto',s1:request.term,sql:sql,minLength:0},callback:function(res){
             response($vm.autocomplete_list(res.table));
@@ -19,7 +19,7 @@ $('#name__ID').focus(function(){$('#name__ID').autocomplete("search","");});
 $('#type__ID').autocomplete({
     minLength:0,
     source:function(request,response){
-        var sql="with tb as (select distinct name=S2 from [TABLE-"+predefined_item_tid+"])";
+        var sql="with tb as (select distinct name=S2 from [TABLE-"+predefined_transaction_item_tid+"])";
         sql+=" select top 10 name,value=name from tb where name like '%'+@S1+'%' ";
         $VmAPI.request({data:{cmd:'auto',s1:request.term,sql:sql,minLength:0},callback:function(res){
             response($vm.autocomplete_list(res.table));
