4d7fe564803c11ef07caf02da84f6c8551231659
Zhiming Zhou <zzm.vmiis@gmail.com>
2017-12-05 15:02:38 +1100

    update

--- templates/survey/index.js
+++ templates/survey/index.js
@@ -173,6 +173,13 @@ var vm_init=function(){
 			$.getScript('https://www.gstatic.com/charts/loader.js',function(){
 				google.charts.load('current', {packages: ['corechart']});
 			});
+			//-------------------------------------
+			$vm.module_list['_system_export_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/dialog/export_dialog_module.html')};
+	        $vm.load_module_by_name('_system_export_dialog_module','',{})
+	        //-------------------------------------
+	        $vm.module_list['_system_import_dialog_module']={table_id:'',url:$vm.url('__COMPONENT__/dialog/import_dialog_module.html')};
+	        $vm.load_module_by_name('_system_import_dialog_module','',{})
+			//-------------------------------------
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
